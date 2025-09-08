38e461b8a64cb623fd57a72b270ceb1a3024f9ea
rahanyas <rahanyas3@gmail.com>
2025-09-08 20:10:21 +0530

    solving the cors error

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 const port = process.env.PORT;
 const app = express();
-const base_url = process.env.NODE_ENV === 'dev' ? 'http://localhost:5173' : "https://job-tracker-green-nu.vercel.app/";
+const base_url = process.env.NODE_ENV === 'dev' ? 'http://localhost:5173' : "https://job-tracker-green-nu.vercel.app";
 
 app.use(cors({
   origin : [base_url, 'https://job-tracker-hb21.onrender.com']
